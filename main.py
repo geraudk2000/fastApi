8450578fdc2da267d72b786a6faa23d15fb9a5fc
Geraud <geraudkameni@gmail.com>
2023-07-02 23:30:22 +0200

    test webserver

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
 from fastapi import FastAPI
+from fastapi import Body
 
 app = FastAPI()
 
 @app.get("/")
-async def root():
-    return {"message": "Hello World"}
+def root():
+    return {"message": "Welcome to my api"}
+
+
+@app.get("/posts")
+def get_posts():
+    return {"data": "This is your posts"}
+
+
+@app.post("/createposts")
+def create_posts(payload: dict = Body(...)):
+    print(payload)
+    return {"message": "Successfuly create a post"}
