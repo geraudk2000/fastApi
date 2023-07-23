62a658c660c3720c990d5e5f660ded1e845b1a6e
Geraud <geraudkameni@gmail.com>
2023-07-23 04:46:07 +0200

    authentification

--- app/oauth2.py
+++ app/oauth2.py
@@ -1,11 +1,13 @@
 from jose import JWSError, jwt
 from datetime import datetime, timedelta
-
+from . import schemas
+from fastapi import Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
 #SECRET_KEY
 #Algorithm 
 #Exploration time
 
-
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
 SECRET_KEY = "c77bd464e45d7bfc0ebaba5f3d5b85a6a78aeb1bf6986e76743270853540936f"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
@@ -19,3 +21,23 @@ def create_access_token(data: dict):
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
 
     return encoded_jwt
+
+def verify_access_token(token: str, credentials_exception):
+
+    try:
+
+        payload = jwt.decode(token, SECRET_KEY, algorithms=ALGORITHM)
+
+        id: str = payload.get("users_id")
+
+        if id is None:
+            raise credentials_exception
+        token_data = schemas.TokenData(id=id)
+    except JWSError: 
+        raise credentials_exception
+    
+def get_current_user(token: str = Depends(oauth2_scheme)):
+    credentials_exception = HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, 
+                                          detail=f"Coudl not validate credentials", headers={"WWW-Authenticate": "Bearer"})
+    
+    return verify_access_token(token, credentials_exception)
--- app/routers/auth.py
+++ app/routers/auth.py
@@ -1,13 +1,15 @@
 from fastapi import APIRouter, Depends, status, HTTPException, Response
 from sqlalchemy.orm import Session
 from .. import database, schemas, models, utils, oauth2
+from fastapi.security.oauth2 import OAuth2PasswordRequestForm
+
 
 router = APIRouter(tags=['Authentification'])
 
 @router.post('/login')
-def login(user_credentials: schemas.UserLogin, db: Session = Depends(database.get_db)):
+def login(user_credentials: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(database.get_db)):
     
-    user = db.query(models.User).filter(models.User.email == user_credentials.email).first()
+    user = db.query(models.User).filter(models.User.email == user_credentials.username).first()
 
     if not user:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Invalid credentials")
