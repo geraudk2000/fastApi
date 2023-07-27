f6aebeb77b0e96f9100836653b3c1f375d8f43ec
Geraud <geraudkameni@gmail.com>
2023-07-27 03:00:20 +0200

    add paramater limit filter search

--- app/database.py
+++ app/database.py
@@ -1,11 +1,18 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+from psycopg2.extras import RealDictCursor
+import psycopg2
+import os
+import time
 
-SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Kcgeraud1986!@localhost/fastapi"
-# SQLALCHEMY_DATABASE_URL = "postgresql://user:password@postgresserver/db"
 
-engine = create_engine(SQLALCHEMY_DATABASE_URL)
+DB_PASSWORD = os.getenv('DB_PASSOWRD')
+DB_URL_DEV = os.getenv('DB_URL_DEV')
+
+#SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Kcgeraud1986!@localhost/fastapi"
+
+engine = create_engine(DB_URL_DEV)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
@@ -16,3 +23,15 @@ def get_db():
         yield db
     finally:
         db.close()
+
+# while True: 
+#     try: 
+#         conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', 
+#                                 password=DB_PASSWORD, cursor_factory=RealDictCursor)
+#         cursor = conn.cursor()
+#         print("Database connection was successfull!")
+#         break
+#     except Exception as error:
+#         print("connecting to database failed")
+#         print("Error: ", error)
+#         time.sleep(2)
--- app/main.py
+++ app/main.py
@@ -1,38 +1,12 @@
-import os
-import time
-from fastapi import FastAPI, Response, status, HTTPException, Depends
-#from fastapi.params import Body
-#from typing import Optional, List
-import psycopg2
-#from . import utils
-#from sqlalchemy.orm import Session
-from psycopg2.extras import RealDictCursor
-from . import models, schemas
-from .database import engine, get_db
+from fastapi import FastAPI
+from . import models
+from .database import engine
 from .routers import post, user, auth
-
-
-DB_PASSWORD = os.getenv('DB_PASSOWRD')
 
 
 models.Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
-
-
-#test the data send to API
-
-while True: 
-    try: 
-        conn = psycopg2.connect(host='localhost', database='fastapi', user='postgres', 
-                                password=DB_PASSWORD, cursor_factory=RealDictCursor)
-        cursor = conn.cursor()
-        print("Database connection was successfull!")
-        break
-    except Exception as error:
-        print("connecting to database failed")
-        print("Error: ", error)
-        time.sleep(2)
 
 app.include_router(post.router)
 app.include_router(user.router)
@@ -41,6 +15,3 @@ app.include_router(auth.router)
 @app.get("/")
 def root():
     return {"message": "Welcome to my api"}
-
-
-
