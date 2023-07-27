7427202bcca57788f78a6ed4a18303967b586d2a
Geraud <geraudkameni@gmail.com>
2023-07-27 04:02:28 +0200

    90 Environment variables

--- app/database.py
+++ app/database.py
@@ -3,16 +3,16 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from psycopg2.extras import RealDictCursor
 import psycopg2
-import os
 import time
+from .config import settings
 
-
-DB_PASSWORD = os.getenv('DB_PASSOWRD')
-DB_URL_DEV = os.getenv('DB_URL_DEV')
+#DB_PASSWORD = os.getenv('DB_PASSOWRD')
+#DB_URL_DEV = os.getenv('DB_URL_DEV')
 
 #SQLALCHEMY_DATABASE_URL = "postgresql://postgres:Kcgeraud1986!@localhost/fastapi"
+SQLALCHEMY_DATABASE_URL = f"postgresql://{settings.database_username}:{settings.database_password}@{settings.database_hostname}:{settings.database_port}/{settings.database_name}"
 
-engine = create_engine(DB_URL_DEV)
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
--- app/oauth2.py
+++ app/oauth2.py
@@ -4,12 +4,13 @@ from sqlalchemy.orm import Session
 from . import schemas, database, models
 from fastapi import Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordBearer
+from .config import settings
 
 
 oauth2_scheme = OAuth2PasswordBearer(tokenUrl='login')
-SECRET_KEY = "c77bd464e45d7bfc0ebaba5f3d5b85a6a78aeb1bf6986e76743270853540936f"
-ALGORITHM = "HS256"
-ACCESS_TOKEN_EXPIRE_MINUTES = 60
+SECRET_KEY = settings.secret_key
+ALGORITHM = settings.algorithm
+ACCESS_TOKEN_EXPIRE_MINUTES = settings.access_token_expire_minutes
 
 def create_access_token(data: dict):
     to_encode = data.copy()
