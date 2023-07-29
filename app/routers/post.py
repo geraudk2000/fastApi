accb93e4f83e3f9e496dd123c35b72e6021c9d32
Geraud <geraudkameni@gmail.com>
2023-07-29 12:35:43 +0200

    correct bug result

--- app/routers/post.py
+++ app/routers/post.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI, Response, status, HTTPException, Depends, APIRouter
 from sqlalchemy.orm import Session
+from sqlalchemy import func
 from typing import Optional, List
 from .. import models, schemas, oauth2
 from ..database import engine, get_db
@@ -9,14 +10,21 @@ router = APIRouter(
     tags=['Posts']
 )
 
-
-@router.get("/", response_model=List[schemas.Post])
+#@router.get("/", response_model=List[schemas.Post])
+@router.get("/")
 def get_posts(db: Session = Depends(get_db), current_user: int = Depends(oauth2.get_current_user), 
               limit: int = 10, skip: int = 0, search: Optional[str] = ""):
     # cursor.execute(""" SELECT * FROM posts """)
     # posts = cursor.fetchall()
     posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
-    return posts
+    
+    test = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip)
+    # results = db.query(models.Post, func.count(models.Vote.post_id)).join(
+    #     models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+
+    results = db.query(models.Post, func.count(models.Vote.post_id).label("votes")).join(models.Vote, models.Vote.post_id == models.Post.id, isouter=True).group_by(models.Post.id).all()
+    results = list(map(lambda x:x._mapping,results))
+    return results
 
 
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.Post)
