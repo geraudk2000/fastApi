cfab061c0e120c8e930e46a3b72ea80b731eadee
Geraud <geraudkameni@gmail.com>
2023-08-17 05:21:43 +0200

    test users with parametrize

--- tests/test_users.py
+++ tests/test_users.py
@@ -4,18 +4,6 @@ from app import schemas
 from .database import client, session
 from app.config import settings
 
-@pytest.fixture
-def test_user(client):
-    user_data = {"email": "hello@gmail.com",
-                 "password": "password123"}
-    
-    res = client.post("/users/", json=user_data)
-
-    assert res.status_code == 201
-    print(res.json())
-    new_user = res.json()
-    new_user['password'] = user_data["password"]
-    return new_user
 
 
 
@@ -44,3 +32,15 @@ def test_login_user(client, test_user):
     assert login_res.token_type == "bearer"
     assert res.status_code == 200
 
+@pytest.mark.parametrize("email, password, status_code", [
+    ('wrongemail@gmail.com', 'password123', 403),
+    ('hello@gmail.com', 'wrongpassword', 403),
+    ('wrongemail@gmail.com', 'wrongpassword', 403),
+    (None, 'password123', 422),
+    ('hello@gmail.com', None, 422)
+] )
+def test_incorrect_login(test_user, client, email, password, status_code):
+    res = client.post("/login", data={"username": email, "password": password})
+
+    assert res.status_code == status_code
+    #assert res.json().get('detail') == 'Invalid Credentials'
