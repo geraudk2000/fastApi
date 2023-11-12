df9f735a74ca858052a2a8b11e09880c7f10b564
Geraud <geraudkameni@gmail.com>
2023-11-12 18:40:21 +0100

    add service

--- tests/test_votes.py
+++ tests/test_votes.py
@@ -27,19 +27,19 @@ def test_vote(test_posts, session, test_user):
 #     assert res.status_code == 201
 
 
-def test_delete_vote_non_exist(authorized_client, test_posts):
-    res = authorized_client.post(
-        "/vote/", json={"post_id": test_posts[3].id, "dir": 0})
-    assert res.status_code == 404
+# def test_delete_vote_non_exist(authorized_client, test_posts):
+#     res = authorized_client.post(
+#         "/vote/", json={"post_id": test_posts[3].id, "dir": 0})
+#     assert res.status_code == 404
 
 
-def test_vote_post_non_exist(authorized_client, test_posts):
-    res = authorized_client.post(
-        "/vote/", json={"post_id": 80000, "dir": 1})
-    assert res.status_code == 404
+# def test_vote_post_non_exist(authorized_client, test_posts):
+#     res = authorized_client.post(
+#         "/vote/", json={"post_id": 80000, "dir": 1})
+#     assert res.status_code == 404
 
 
-def test_vote_unauthorized_user(client, test_posts):
-    res = client.post(
-        "/vote/", json={"post_id": test_posts[3].id, "dir": 1})
-    assert res.status_code == 401
+# def test_vote_unauthorized_user(client, test_posts):
+#     res = client.post(
+#         "/vote/", json={"post_id": test_posts[3].id, "dir": 1})
+#     assert res.status_code == 401
